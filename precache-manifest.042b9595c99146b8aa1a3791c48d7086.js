e3ba8337d17fd96bf947e4d00c22b177fd5b86b4
Mattan1999 <mattias470@gmail.com>
2021-05-12 15:31:09 +0200

    Added built vue files

---
+++ precache-manifest.042b9595c99146b8aa1a3791c48d7086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca13234d7556069c9c9",
+    "url": "/css/app.4cd06cec.css"
+  },
+  {
+    "revision": "7781faec2b6710137f9da54846fccf5f",
+    "url": "/img/laugh.7781faec.png"
+  },
+  {
+    "revision": "bf0b1be9f0a2798988470aceece5e3b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca13234d7556069c9c9",
+    "url": "/js/app.66110985.js"
+  },
+  {
+    "revision": "d51882857e01de99cdb0",
+    "url": "/js/chunk-vendors.fe4e27e6.js"
+  },
+  {
+    "revision": "0c81d8432a8dc5688ac2606f499d0cff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
